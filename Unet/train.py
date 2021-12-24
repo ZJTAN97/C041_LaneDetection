3a3a274e53707c4566dd3a911d692bd9beaec93c
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2021-12-24 15:43:03 +0800

    added pre-commit

--- Unet/train.py
+++ Unet/train.py
@@ -12,29 +12,41 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import torch
 import time
-import os
-
 
 
 image_paths = sorted(list(paths.list_images(config.IMAGE_DATATSET_PATH)))
 mask_paths = sorted(list(paths.list_images(config.MASK_DATASET_PATH)))
 
-split = train_test_split(image_paths, mask_paths, test_size=config.TEST_SPLIT, random_state=42)
+split = train_test_split(
+    image_paths, mask_paths, test_size=config.TEST_SPLIT, random_state=42
+)
 
 (train_images, test_images) = split[:2]
 (train_masks, test_masks) = split[2:]
 
-print('[INFO] saving testing image paths...')
+print("[INFO] saving testing image paths...")
 f = open(config.TEST_PATHS, "w")
 f.write("\n".join(test_images))
 f.close()
 
 
 # Data Loading Pipeline
-transforms = transforms.Compose([transforms.ToPILImage(), transforms.Resize((config.INPUT_IMAGE_HEIGHT, config.INPUT_IMAGE_WIDTH)), transforms.ToTensor()])
+transforms = transforms.Compose(
+    [
+        transforms.ToPILImage(),
+        transforms.Resize(
+            (config.INPUT_IMAGE_HEIGHT, config.INPUT_IMAGE_WIDTH)
+        ),
+        transforms.ToTensor(),
+    ]
+)
 
-train_ds = LaneDetectionDataset(image_paths=train_images, mask_paths=train_masks, transforms=transforms)
-test_ds = LaneDetectionDataset(image_paths=test_images, mask_paths=test_masks, transforms=transforms)
+train_ds = LaneDetectionDataset(
+    image_paths=train_images, mask_paths=train_masks, transforms=transforms
+)
+test_ds = LaneDetectionDataset(
+    image_paths=test_images, mask_paths=test_masks, transforms=transforms
+)
 
 print(f"[INFO] found {len(train_ds)} examples in the training set...")
 print(f"[INFO] found {len(test_ds)} examples in the test set...")
@@ -45,13 +57,13 @@ test_loader = DataLoader(test_ds, shuffle=False, batch_size=config.BATCH_SIZE)
 
 # Initialize UNet Model
 unet = UNet().to(config.DEVICE)
-loss_fn = BCEWithLogitsLoss() # implement diceloss later
+loss_fn = BCEWithLogitsLoss()  # implement diceloss later
 dice_loss_fn = DiceBCELoss()
 optimizer = Adam(unet.parameters(), lr=config.INIT_LR)
 
 train_steps = len(train_ds) // config.BATCH_SIZE
 test_steps = len(test_ds) // config.BATCH_SIZE
-history = { "train_loss": [], "test_loss": [] }
+history = {"train_loss": [], "test_loss": []}
 
 
 # Training Loop
@@ -77,7 +89,7 @@ for e in tqdm(range(config.NUM_EPOCHS)):
         optimizer.step()
 
         total_train_loss += loss
-    
+
     # switch off autograd
     with torch.no_grad():
         # set model in evaluation mode
@@ -88,23 +100,30 @@ for e in tqdm(range(config.NUM_EPOCHS)):
             pred = unet(x)
             total_test_loss += dice_loss_fn(pred, y)
 
-    
     avg_train_loss = total_train_loss / train_steps
     avg_test_loss = total_test_loss / test_steps
 
-    history['train_loss'].append(avg_train_loss.cpu().detach().numpy())
-    history['test_loss'].append(avg_test_loss.cpu().detach().numpy())
+    history["train_loss"].append(avg_train_loss.cpu().detach().numpy())
+    history["test_loss"].append(avg_test_loss.cpu().detach().numpy())
 
-    print("[INFO] EPOCH: {}/{}".format(e+1, config.NUM_EPOCHS))
-    print("Train loss: {:.6f}, Test loss: {:.6f}".format(avg_train_loss, avg_test_loss))
-    torch.save({
-        "state_dict": unet.state_dict(),
-        "optimizer": optimizer.state_dict()
-    }, config.MODEL_PATH)
-    print('saved weights successfully!')
+    print("[INFO] EPOCH: {}/{}".format(e + 1, config.NUM_EPOCHS))
+    print(
+        "Train loss: {:.6f}, Test loss: {:.6f}".format(
+            avg_train_loss, avg_test_loss
+        )
+    )
+    torch.save(
+        {"state_dict": unet.state_dict(), "optimizer": optimizer.state_dict()},
+        config.MODEL_PATH,
+    )
+    print("saved weights successfully!")
 
 end_time = time.time()
-print("[INFO] total time taken to trian the model: {:.2f}s".format(end_time-start_time))
+print(
+    "[INFO] total time taken to trian the model: {:.2f}s".format(
+        end_time - start_time
+    )
+)
 
 
 plt.style.use("ggplot")
@@ -117,4 +136,3 @@ plt.ylabel("Loss")
 plt.legend(loc="lower left")
 plt.show()
 plt.savefig(config.PLOT_PATH)
-
