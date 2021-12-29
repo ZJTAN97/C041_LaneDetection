9a301569c782e3d3322861bd1aa3d0b2355c6299
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2021-12-29 11:54:05 +0800

    updated readme

--- Enet/predict.py
+++ Enet/predict.py
@@ -56,9 +56,54 @@ def make_predictions(image_path):
         cv.waitKey(0)
 
 
-print("[INFO] loading up test image paths....")
-image_paths = open(config.TEST_PATHS).read().strip().split("\n")
-image_paths = np.random.choice(image_paths, size=10)
+# print("[INFO] loading up test image paths....")
+# image_paths = open(config.TEST_PATHS).read().strip().split("\n")
+# image_paths = np.random.choice(image_paths, size=10)
 
-for path in image_paths:
-    make_predictions(path)
+# for path in image_paths:
+#     make_predictions(path)
+
+
+def make_predictions_video(video_path):
+
+    model = ENet(1)
+    checkpoint = torch.load(
+        config.PRE_TRAINED_WEIGHTS_PATH, map_location=torch.device("cpu")
+    )
+    model.load_state_dict(checkpoint["state_dict"])
+
+    cap = cv.VideoCapture(video_path)
+
+    while True:
+
+        with torch.no_grad():
+            success, frame = cap.read()
+            frame = cv.resize(
+                frame, (config.INPUT_IMAGE_WIDTH, config.INPUT_IMAGE_HEIGHT)
+            )
+            orig = frame.copy()
+            frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+            frame = frame.astype("float32") / 255.0
+
+            frame = np.transpose(frame, (2, 0, 1))
+            frame = np.expand_dims(frame, 0)
+            frame = torch.from_numpy(frame).to(config.DEVICE)
+
+            pred = model(frame).squeeze()
+            pred = torch.sigmoid(pred)
+            pred = pred.cpu().numpy()
+
+            pred = (pred > config.THRESHOLD) * 255
+            pred = pred.astype(np.uint8)
+
+            pred_colored = cv.cvtColor(pred, cv.COLOR_GRAY2RGB)
+            stacked = np.concatenate((orig, pred_colored), axis=1)
+
+            cv.imshow("Comparison", stacked)
+
+            if cv.waitKey(1) & 0xFF == ord("q"):
+                break
+
+
+path = "../dataset/test_videos/test_video_2.mp4"
+make_predictions_video(path)
--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-## To Preprocess Image
+# C041 - Lane Detection
+
+This repository contains my work for my Final Year Project, C041 Lane Detection. The repository can be further
+broken down into the following segments. The entire project is under constant updates and will change in the near future.
+
+1. U Net
+    - config: required configurations for training and prediction
+    - model: the architecture of U Net written in PyTorch
+    - output: consists of the loss plots and pre-trained weights
+    - train.py: to start training using U Net architecture.
+    - predict.py: to predict/test pre-trained weights using U Net architecture
+
+2. E Net
+    - config: required configurations for training and prediction
+    - model: the architecture of U Net written in PyTorch
+    - output: consists of the loss plots and pre-trained weights
+    - train.py: to start training using E Net architecture.
+    - predict.py: to predict/test pre-trained weights using E Net architecture
+
+3. Lane Detection
+    - currently in major development
+    - consists of integration of DJI tellopy API and trained weights.
+    - Responsible for drone lane detection
+
+4. Dataset
+    - matlab_labels
+    - sample_mask
+    - sample_train
+    - scripts
+    - test_videos
+    - train_imgs
+    - train_masks
+    - unprocessed
+
+
+# Getting Started
+
+## To add new Images to dataset.
 1. Use MATLAB Image segmenter
 2. Draw out the polygon, apply binary on the segmenter, export the masked Image
 3. Select all the maskedImaged, save as a matlab matrix `.mat`
